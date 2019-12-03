640c2d95c3387d3e9d2c47e3fa7757ce86fdea55
Matt Bedell <matt_bedell@condenast.com>
2019-12-03 16:30:18 -0500

    add megatab

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
@@ -3,6 +3,34 @@
 
 LEADER_EXTERNS();
 
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+// Define a type for as many tap dance states as you need
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+	DOUBLE_HOLD = 4,
+	TRIPLE_TAP = 5,
+	TRIPLE_HOLD = 6,
+};
+
+enum {
+  MEGA_TAB = 0
+};
+
+// Declare the functions to be used with your tap dance key(s)
+
+//Function associated with all tap dances
+int cur_dance (qk_tap_dance_state_t *state);
+
+//Functions associated with individual tap dances
+void mt_finished (qk_tap_dance_state_t *state, void *user_data);
+void mt_reset (qk_tap_dance_state_t *state, void *user_data);
+
 
 void matrix_scan_user(void) {
   LEADER_DICTIONARY() {
@@ -125,17 +153,96 @@ void matrix_scan_user(void) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_all(
-		LM(1, MOD_LGUI),             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_BSPC,  KC_HOME,
-		LT(1, KC_TAB),              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,           KC_BSLS,  TO(1),
+		KC_GRV,             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_BSPC,  KC_HOME,
+		TD(MEGA_TAB),              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,           KC_BSLS,  TO(1),
 		LCTL_T(KC_ESC),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,    KC_QUOT,          KC_ENT,             TO(0),
 		KC_LSPO,         KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,    KC_N,    KC_M, KC_COMM,          KC_DOT,    KC_SLSH, KC_RSPC,             KC_UP,    LCTL(LGUI(KC_Q)),
-		HYPR_T(KC_ESC), LALT_T(KC_CAPS), KC_LGUI,   KC_NO,                          KC_SPC,             KC_NO,         RGUI_T(KC_UNDS),    KC_LEAD,         KC_LEFT,   KC_DOWN,   KC_RGHT
+		HYPR_T(KC_ESC), LALT_T(KC_CAPS), KC_LGUI,   KC_NO,                          KC_SPC,             KC_NO,         KC_RGUI,    KC_LEAD,         KC_LEFT,   KC_DOWN,   KC_RGHT
 	),
 	[1] = LAYOUT_all(
-		KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,       _______,
 		_______, _______, _______, _______, KC_PGDN, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______,                _______,
 		_______, _______, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______,             HYPR(KC_K), _______,
 		_______, _______, _______,          _______,          KC_TAB,          _______,            KC_GRV, _______,          HYPR(KC_H), HYPR(KC_J), HYPR(KC_L)
 	)
+};
+
+// Determine the current tap dance state
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (!state->pressed) {
+      return SINGLE_TAP;
+    } else {
+      return SINGLE_HOLD;
+    }
+  } else if (state->count == 2) {
+		if (!state->pressed) {
+			return DOUBLE_TAP;
+		} else {
+			return DOUBLE_HOLD;
+		}
+  } else if (state->count == 3) {
+		if (!state->pressed) {
+			return TRIPLE_TAP;
+		} else {
+			return TRIPLE_HOLD;
+		}
+	}
+  else return 8;
+}
+
+//Initialize tap structure associated with example tap dance key
+static tap mt_tap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+//Functions that control what our tap dance key does
+void mt_finished (qk_tap_dance_state_t *state, void *user_data) {
+  mt_tap_state.state = cur_dance(state);
+  switch (mt_tap_state.state) {
+    case SINGLE_TAP:
+      tap_code(KC_TAB);
+      break;
+    case SINGLE_HOLD:
+      layer_on(1);
+      break;
+    case DOUBLE_TAP:
+			SEND_STRING(SS_LSFT("_"));
+      break;
+    case DOUBLE_HOLD:
+			layer_on(1);
+			register_code16(KC_LGUI);
+      break;
+		case TRIPLE_HOLD:
+			register_code(KC_LSFT);
+			register_code(KC_LALT);
+			register_code(KC_LGUI);
+			register_code(KC_LCTL);
+			break;
+  }
+}
+
+void mt_reset (qk_tap_dance_state_t *state, void *user_data) {
+  //if the key was held down and now is released then switch off the layer
+  if (mt_tap_state.state==SINGLE_HOLD) {
+    layer_off(1);
+  }
+
+  if (mt_tap_state.state==DOUBLE_HOLD) {
+		clear_keyboard();
+    layer_off(1);
+  }
+
+  if (mt_tap_state.state==TRIPLE_HOLD) {
+		clear_keyboard();
+  }
+
+  mt_tap_state.state = 0;
+}
+
+//Associate our tap dance key with its functionality
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [MEGA_TAB] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, mt_finished, mt_reset, 275)
 };
