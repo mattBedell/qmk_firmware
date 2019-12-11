2326967c1428b6f988d21635dbc0fe9fc30bc3ec
Matt Bedell <matt@bedell.co>
2019-12-10 20:47:32 -0500

    move keys for dev/tty keycaps

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
@@ -190,10 +190,10 @@ void matrix_scan_user(void) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
-    KC_GRV,        KC_1,            KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_BSPC,      KC_HOME,
-    TD(MEGA_TAB),  KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               KC_BSLS,      TO(1),
-    TD(MEGA_CAPS), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          TD(MEGA_ENT),               TO(0),
-    KC_LSPO,       KC_LSPO,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,               KC_UP,        LCTL(LGUI(KC_Q)),
+    KC_GRV,        KC_1,            KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_BSPC,      LCTL(LGUI(KC_Q)),
+    TD(MEGA_TAB),  KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               KC_BSLS,      KC_HOME,
+    TD(MEGA_CAPS), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          TD(MEGA_ENT),               TO(1),
+    KC_LSPO,       KC_LSPO,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,               KC_UP,        TO(0),
     KC_LGUI,       LALT_T(KC_CAPS), KC_LGUI,           KC_NO,            KC_SPC,           KC_NO,            KC_RGUI, KC_LEAD,          KC_LEFT,      KC_DOWN,      KC_RGHT
   ),
   [1] = LAYOUT_all(
