1a4be4111b3a8dbcd9d329a26048c67fe7478a0c
Matt Bedell <matt_bedell@condenast.com>
2020-02-06 14:18:29 -0500

    remove mega caps and replace with mod hold

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
@@ -20,7 +20,6 @@ enum {
 
 enum {
   MEGA_TAB = 0,
-  MEGA_CAPS,
   MEGA_BSLS,
   MEGA_LSFT,
 };
@@ -33,9 +32,6 @@ int cur_dance (qk_tap_dance_state_t *state);
 void mega_tab_finished (qk_tap_dance_state_t *state, void *user_data);
 void mega_tab_reset (qk_tap_dance_state_t *state, void *user_data);
 
-void mega_caps_finished (qk_tap_dance_state_t *state, void *user_data);
-void mega_caps_reset (qk_tap_dance_state_t *state, void *user_data);
-
 void mega_bsls_finished (qk_tap_dance_state_t *state, void *user_data);
 void mega_bsls_reset (qk_tap_dance_state_t *state, void *user_data);
 
@@ -45,11 +41,11 @@ void mega_lsft_reset (qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
-    KC_GRV,        KC_1,            KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_BSPC,       KC_HOME,
-    TD(MEGA_TAB),  KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               TD(MEGA_BSLS), LGUI(LALT(KC_X)),
-    TD(MEGA_CAPS), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                      LGUI(LALT(KC_Z)),
-    TD(MEGA_LSFT), TD(MEGA_LSFT),   KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,               KC_UP,         LGUI(LALT(KC_C)),
-    KC_CAPS,       KC_LALT,         KC_LGUI,           KC_NO,            KC_SPC,           KC_NO,            KC_RGUI, KC_RALT,          KC_LEFT,      KC_DOWN,      KC_RGHT
+    KC_GRV,         KC_1,            KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_BSPC,       KC_HOME,
+    TD(MEGA_TAB),   KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               TD(MEGA_BSLS), LGUI(LALT(KC_X)),
+    LCTL_T(KC_ESC), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                      LGUI(LALT(KC_Z)),
+    TD(MEGA_LSFT),  TD(MEGA_LSFT),   KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,               KC_UP,         LGUI(LALT(KC_C)),
+    KC_CAPS,        KC_LALT,         KC_LGUI,           KC_NO,            KC_SPC,           KC_NO,            KC_RGUI, KC_RALT,          KC_LEFT,      KC_DOWN,      KC_RGHT
   ),
   [1] = LAYOUT_all(
     _______,   KC_F1, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LCTL(LGUI(KC_Q)),
@@ -89,11 +85,6 @@ static tap mega_tab_tap_state = {
   .state = 0
 };
 
-static tap mega_caps_tap_state = {
-  .is_press_action = true,
-  .state = 0
-};
-
 static tap mega_bsls_tap_state = {
   .is_press_action = true,
   .state = 0
@@ -127,25 +118,6 @@ void mega_tab_reset (qk_tap_dance_state_t *state, void *user_data) {
     clear_keyboard();
   }
   mega_tab_tap_state.state = 0;
-}
-
-void mega_caps_finished (qk_tap_dance_state_t *state, void *user_data) {
-  mega_caps_tap_state.state = cur_dance(state);
-  switch (mega_caps_tap_state.state) {
-    case SINGLE_TAP:
-      tap_code(KC_ESC);
-      break;
-    case SINGLE_HOLD:
-      register_code16(KC_LCTL);
-      break;
-  }
-}
-
-void mega_caps_reset (qk_tap_dance_state_t *state, void *user_data) {
-  if (mega_caps_tap_state.state==SINGLE_HOLD) {
-    clear_keyboard();
-  }
-  mega_caps_tap_state.state = 0;
 }
 
 void mega_bsls_finished (qk_tap_dance_state_t *state, void *user_data) {
@@ -196,7 +168,6 @@ void mega_lsft_reset (qk_tap_dance_state_t *state, void *user_data) {
 //Associate our tap dance key with its functionality
 qk_tap_dance_action_t tap_dance_actions[] = {
   [MEGA_TAB] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, mega_tab_finished, mega_tab_reset, 275),
-  [MEGA_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mega_caps_finished, mega_caps_reset),
   [MEGA_BSLS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mega_bsls_finished, mega_bsls_reset),
   [MEGA_LSFT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, mega_lsft_finished, mega_lsft_reset, 275)
 };
