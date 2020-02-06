daca00e952065f48cf1d5bbbd88da1be6365f0cd
Matt Bedell <matt_bedell@condenast.com>
2020-02-06 13:00:46 -0500

    move capslock to rctrl

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TD(MEGA_TAB),  KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               KC_BSLS,      LGUI(LALT(KC_X)),
     TD(MEGA_CAPS), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          TD(MEGA_ENT),               LGUI(LALT(KC_Z)),
     KC_LSPO,       KC_LSPO,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,               KC_UP,        LGUI(LALT(KC_C)),
-    KC_LGUI,       LALT_T(KC_CAPS), KC_LGUI,           KC_NO,            KC_SPC,           KC_NO,            KC_RGUI, KC_RALT,          KC_LEFT,      KC_DOWN,      KC_RGHT
+    KC_CAPS,       KC_LALT,         KC_LGUI,           KC_NO,            KC_SPC,           KC_NO,            KC_RGUI, KC_RALT,          KC_LEFT,      KC_DOWN,      KC_RGHT
   ),
   [1] = LAYOUT_all(
     _______,   KC_F1, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LCTL(LGUI(KC_Q)),
