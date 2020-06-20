9dd6cb1749525b6dcbb81e687fc6c2f1724007a4
Matt Bedell <matt@bedell.co>
2020-06-19 20:56:45 -0400

    add megashift, remove megatab because of tab delay

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
@@ -19,8 +19,8 @@ enum {
 };
 
 enum {
-  MEGA_TAB = 0,
-  MEGA_BSLS,
+  MEGA_BSLS = 0,
+  MEGA_SHIFT,
 };
 
 
@@ -28,19 +28,18 @@ enum {
 int cur_dance (qk_tap_dance_state_t *state);
 
 // Functions associated with individual tap dances
-void mega_tab_finished (qk_tap_dance_state_t *state, void *user_data);
-void mega_tab_reset (qk_tap_dance_state_t *state, void *user_data);
-
 void mega_bsls_finished (qk_tap_dance_state_t *state, void *user_data);
 void mega_bsls_reset (qk_tap_dance_state_t *state, void *user_data);
 
+void mega_shift_finished (qk_tap_dance_state_t *state, void *user_data);
+void mega_shift_reset (qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
     KC_GRV,         KC_1,            KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_BSPC,       KC_HOME,
-    TD(MEGA_TAB),   KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               TD(MEGA_BSLS), LGUI(LALT(KC_X)),
+    LT(1, KC_TAB),  KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               TD(MEGA_BSLS), LGUI(LALT(KC_X)),
     LCTL_T(KC_ESC), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                      LGUI(LALT(KC_Z)),
-    KC_LSFT,        KC_LSFT,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,               KC_UP,         LGUI(LALT(KC_C)),
+    TD(MEGA_SHIFT), KC_LSFT,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,               KC_UP,         LGUI(LALT(KC_C)),
     KC_CAPS,        KC_LALT,         KC_LGUI,           KC_NO,            KC_SPC,           KC_NO,            KC_RGUI, KC_RALT,          KC_LEFT,      KC_DOWN,      KC_RGHT
   ),
   [1] = LAYOUT_all(
@@ -76,7 +75,7 @@ int cur_dance (qk_tap_dance_state_t *state) {
 }
 
 // Initialize tap structure associated with tap dance key
-static tap mega_tab_tap_state = {
+static tap mega_shift_tap_state = {
   .is_press_action = true,
   .state = 0
 };
@@ -85,35 +84,6 @@ static tap mega_bsls_tap_state = {
   .is_press_action = true,
   .state = 0
 };
-
-
-void mega_tab_finished (qk_tap_dance_state_t *state, void *user_data) {
-  mega_tab_tap_state.state = cur_dance(state);
-  switch (mega_tab_tap_state.state) {
-    case SINGLE_TAP:
-      tap_code(KC_TAB);
-      break;
-    case SINGLE_HOLD:
-      layer_on(1);
-      break;
-    case DOUBLE_HOLD:
-      register_code(KC_LSFT);
-      register_code(KC_LALT);
-      register_code(KC_LGUI);
-      register_code(KC_LCTL);
-      break;
-  }
-}
-
-void mega_tab_reset (qk_tap_dance_state_t *state, void *user_data) {
-  if (mega_tab_tap_state.state==SINGLE_HOLD) {
-    layer_off(1);
-  }
-  if (mega_tab_tap_state.state==DOUBLE_HOLD) {
-    clear_keyboard();
-  }
-  mega_tab_tap_state.state = 0;
-}
 
 void mega_bsls_finished (qk_tap_dance_state_t *state, void *user_data) {
   mega_bsls_tap_state.state = cur_dance(state);
@@ -136,8 +106,35 @@ void mega_bsls_reset (qk_tap_dance_state_t *state, void *user_data) {
   mega_bsls_tap_state.state = 0;
 }
 
+void mega_shift_finished (qk_tap_dance_state_t *state, void *user_data) {
+  mega_shift_tap_state.state = cur_dance(state);
+  switch (mega_shift_tap_state.state) {
+    case SINGLE_HOLD:
+      register_code(KC_LSFT);
+      break;
+    case DOUBLE_HOLD:
+      register_code(KC_LSFT);
+      register_code(KC_LALT);
+      register_code(KC_LGUI);
+      register_code(KC_LCTL);
+      break;
+  }
+}
+
+void mega_shift_reset (qk_tap_dance_state_t *state, void *user_data) {
+  if (mega_shift_tap_state.state==SINGLE_HOLD) {
+    clear_keyboard();
+  }
+  if (mega_shift_tap_state.state==DOUBLE_HOLD) {
+    clear_keyboard();
+  }
+  mega_shift_tap_state.state = 0;
+}
+
+
 //Associate our tap dance key with its functionality
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [MEGA_TAB] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, mega_tab_finished, mega_tab_reset, 275),
   [MEGA_BSLS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mega_bsls_finished, mega_bsls_reset),
+  [MEGA_SHIFT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, mega_shift_finished, mega_shift_reset, 275),
 };
+
