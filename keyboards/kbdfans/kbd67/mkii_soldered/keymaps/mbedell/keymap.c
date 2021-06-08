c2f98619ec8822078a8002ca94c2d654aeb87f95
Matt Bedell <matt_bedell@condenast.com>
2021-06-08 11:37:13 -0400

    Map page up/down keys

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/mbedell/keymap.c
@@ -37,9 +37,9 @@ void mega_shift_reset (qk_tap_dance_state_t *state, void *user_data);
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
     KC_GRV,         KC_1,            KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_BSPC,       KC_HOME,
-    LT(1, KC_TAB),  KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               TD(MEGA_BSLS), LGUI(LALT(KC_X)),
-    LCTL_T(KC_ESC), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                      LGUI(LALT(KC_Z)),
-    TD(MEGA_SHIFT), KC_LSFT,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,               KC_UP,         LGUI(LALT(KC_C)),
+    LT(1, KC_TAB),  KC_Q,            KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,               TD(MEGA_BSLS), KC_PGUP,
+    LCTL_T(KC_ESC), KC_A,            KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                      KC_PGDN,
+    TD(MEGA_SHIFT), KC_LSFT,         KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,               KC_UP,         KC_END,
     KC_CAPS,        KC_LALT,         KC_LGUI,           KC_NO,            KC_SPC,           KC_NO,            KC_RGUI, KC_RALT,          KC_LEFT,      KC_DOWN,      KC_RGHT
   ),
   [1] = LAYOUT_all(
